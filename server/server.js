4fbdc04d78dc4e4b24d2469bff74c9c389680542
Prakhar Gussain <prakhargussain069@gmail.com>
2025-09-23 01:46:18 +0530

    server created

---
+++ server/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// import connectDB from "./config/dbconnection.js";
+// import authRoutes from "./routes/Authroutes.js"; // renamed for clarity
+import cookieParser from "cookie-parser";
+// import Reportroutes from "./routes/Reportroutes.js"
+// import Verifyreport from "./routes/verifyreport.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = 5002;
+app.use(cookieParser());
+app.use(express.json());
+
+// Public route
+app.get("/", (req, res) => {
+  res.send("🚀 API Running with MongoDB (no Clerk)");
+});
+
+// API routes
+// app.use("/api", authRoutes);
+// app.use("/api",Reportroutes);
+// app.use("/api",Verifyreport)
+
+
+// Start server only after DB connects
+const startServer = async () => {
+  try {
+    // await connectDB(); // ensure DB is connected
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on: http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to connect to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
